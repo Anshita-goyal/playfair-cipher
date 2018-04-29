04d552165486ef5260573c9c594e5ef193b07b4d
Sana Riaz <sanariaz154@gmail.com>
2018-04-29 17:35:25 +0500

    Update playfair.py

--- playfair.py
+++ playfair.py
@@ -16,10 +16,15 @@ enc={row[i]+row[j]:row[(i+1)%5]+row[(j+1)%5] for row in m for i,j in d(range(5),
 enc.update({col[i]+col[j]:col[(i+1)%5]+col[(j+1)%5] for col in zip(*m) for i,j in d(range(5),repeat=2) if i!=j})
 enc.update({m[i1][j1]+m[i2][j2]:m[i1][j2]+m[i2][j1] for i1,j1,i2,j2 in d(range(5),repeat=4) if i1!=i2 and j1!=j2})
 dec = dict((v, key) for key, v in enc.items())
+
+#encoding
 l=re.findall(r'(.)(?:(?!\1)(.))?',''.join([_ for _ in t(text) if _ in asc]))
 encoded=''.join(enc[a+(b if b else 'X')] for a,b in l)
+
+#decoding
 f=re.findall(r'(.)(?:(?!\1)(.))?',''.join([_ for _ in t(encoded) if _ in asc]))
 decoded=''.join(dec[a+(b if b else 'X')] for a,b in f)
+
 print("\n_____________________\n\nEncoded: ",encoded,"\nDecoded: ",decoded)
 
 
